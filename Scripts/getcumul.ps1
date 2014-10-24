edaa9f3f59f2b5a8f77f0e9ad7d57fc54e9699fd
stefmsft <stefmsft@live.com>
2014-10-24 11:10:04 +0200

    CleanUp Project

--- Scripts/getcumul.ps1
+++
@@ -1,7 +0,0 @@
-[reflection.assembly]::LoadWithPartialName("TrackingObject") > $null
-$TC = new-object TrackingObject.TrackingCol
-$Today =  [DateTime]::now
-$TC.End=$Today.ToShortDateString()
-$TC.Start=$Today.AddMonths(-1).ToShortDateString()
-$TC.Extract() > $null
-$TC.Categories["mssa-Activity"] | ForEach-Object {$_.Cumul} | sort
